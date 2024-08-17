0a7125bd9e93566684528ca9e8cb29f219e10bb7
Ravivarma2003 <ravivarmapalla2020@gmail.com>
2024-08-17 15:59:39 +0530

    added new condition

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
     expectedReturn: 5,
     duration: 10
 })
+
+const inputIsValid = userInput.duration >= 1;
+
 function handleChange(inputIdentifier, newValue) {
   setUserInput(prevUserInput => {
       return {
@@ -19,11 +22,15 @@ function handleChange(inputIdentifier, newValue) {
       }
   });
 }
+
+
+
   return (
     <>
     <Header />
     <UserInput userInput={userInput} onChange={handleChange}/>
-    <Results input={userInput}/>
+    {!inputIsValid && <p className='center'>Please enter duration greater than zero</p>}
+    {inputIsValid && <Results input={userInput}/>}
     </>
   )
 }
